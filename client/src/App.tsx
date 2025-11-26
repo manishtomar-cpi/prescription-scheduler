1e737f227d947c664dce0ad0ed0a1b7239b7034a
Manish Tomar <Manish.Tomar@uk-cpi.com>
2025-11-26 06:46:06 +0000

    chore(client): setup react and typescript application

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-sky-400">
+        Prescription Scheduler – Client
+      </h1>
+    </div>
+  );
+}
+
+export default App;
